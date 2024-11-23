3a5f33cc4d667d054446f91612acc52fdab6ae74
MeghdadFadaee <mmf9484943@gmail.com>
2024-11-23 10:44:34 +0330

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::any('blockout', function (){
+  
   $url = 'https://qepd.co.ir/fa-IR/DouranPortal/6423';
   @$content = file_get_contents($url);
   return $content;
