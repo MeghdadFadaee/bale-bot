3291581ff0bc4810102d744d24ca081999c3a970
Meghdad <mmf9484943@gmail.com>
2024-11-23 11:42:43 +0330

    remove blackout

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-
-Route::any('blockout', function (){
-  
-  $url = 'https://qepd.co.ir/fa-IR/DouranPortal/6423';
-  @$content = file_get_contents($url);
-  return $content;
-});
