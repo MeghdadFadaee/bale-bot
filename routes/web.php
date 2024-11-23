b741bd09a88929cd8ec9529d35b3e399d453ac05
MeghdadFadaee <mmf9484943@gmail.com>
2024-11-23 09:01:07 +0330

    add blockout page

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+Route::any('blockout', function (){
+  $url = 'https://qepd.co.ir/fa-IR/DouranPortal/6423';
+  @$content = file_get_contents($url);
+  return $content;
+});
